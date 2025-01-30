962c2b0704eaee8e4e6c08344e66ef860fe801ad
uddeshyasonkar <67516897+uddeshyasonkar@users.noreply.github.com>
2025-01-30 12:37:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,14 +6,23 @@ import bodyParser from "body-parser";
 import connectDB from "./src/config/db.js";
 import formDataRoutes from "./src/routes/formDataRoutes.js";
 import packageRoutes from "./src/routes/packageRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8090;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 connectDB();
 
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use(cors());
 app.use(express.json());
 
