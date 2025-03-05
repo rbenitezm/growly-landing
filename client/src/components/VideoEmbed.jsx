be39c1535b6316e2569e82e814ad1f777a89f474
SinghHridyansh <hridyansh.360@gmail.com>
2025-03-05 18:39:06 +0530

    Video player updated - wista

--- client/src/components/VideoEmbed.jsx
+++ client/src/components/VideoEmbed.jsx
@@ -4,24 +4,24 @@ import React from "react";
 const VideoEmbed = () => {
   const iframeRef = useRef(null);
 
-  useEffect(() => {
-    const unmuteVideo = () => {
-      const iframe = iframeRef.current;
-      if (iframe) {
-        const src = iframe.src.replace("&mute=1", ""); // Remove mute parameter
-        iframe.src = src; // Reload iframe to apply changes
-      }
-      document.removeEventListener("click", unmuteVideo); // Remove event listener after first click
-    };
+  // useEffect(() => {
+  //   const unmuteVideo = () => {
+  //     const iframe = iframeRef.current;
+  //     if (iframe) {
+  //       const src = iframe.src.replace("&mute=1", ""); // Remove mute parameter
+  //       iframe.src = src; // Reload iframe to apply changes
+  //     }
+  //     document.removeEventListener("click", unmuteVideo); // Remove event listener after first click
+  //   };
 
-    document.addEventListener("click", unmuteVideo); // Wait for user interaction
-  }, []);
+  //   document.addEventListener("click", unmuteVideo); // Wait for user interaction
+  // }, []);
   return (
     <div
       className="max-w-[90vw] h-max md:h-auto md:w-5/6 lg:w-full lg:max-w-[70vw] flex justify-center items-center box-border "
       style={{ width: "90%" }}
     >
-      <iframe
+      {/* <iframe
         ref={iframeRef}
         width="560"
         height="315"
@@ -32,12 +32,41 @@ const VideoEmbed = () => {
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
         className="w-full p-0 h-[25vh] md:h-80 md:w-5/6 lg:w-full max-w-[90vw] lg:max-w-[45vw] lg:h-[50vh] rounded-xl overflow-hidden"
+      ></iframe> */}
+
+      <iframe
+        src="https://fast.wistia.net/embed/iframe/7ilkqh3alv"
+        title="Wistia Video"
+        allow="autoplay; fullscreen"
+        allowFullScreen
+        className="w-full p-0 h-[25vh] md:h-80 md:w-5/6 lg:w-full max-w-[90vw] lg:max-w-[45vw] lg:h-[50vh] rounded-xl overflow-hidden "
       ></iframe>
     </div>
   );
 };
 
 export default VideoEmbed;
+
+// return (
+//   <div
+//     className="max-w-[90vw] h-max md:h-auto md:w-5/6 lg:w-full lg:max-w-[70vw] flex justify-center items-center box-border "
+//     style={{ width: "90%" }}
+//   >
+//     <iframe
+//       ref={iframeRef}
+//       width="560"
+//       height="315"
+//       src="https://www.youtube.com/embed/sn3GfVmfu_4?autoplay=1&mute=1&si=uEUvYJaWEugszLVX"
+//       title="YouTube video player"
+//       frameBorder="0"
+//       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//       referrerPolicy="strict-origin-when-cross-origin"
+//       allowFullScreen
+//       className="w-full p-0 h-[25vh] md:h-80 md:w-5/6 lg:w-full max-w-[90vw] lg:max-w-[45vw] lg:h-[50vh] rounded-xl overflow-hidden"
+//     ></iframe>
+//   </div>
+// );
+// };
 
 {
   /* <iframe
